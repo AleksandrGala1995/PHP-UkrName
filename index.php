c7f75a80d7f63274849734a311a7d270ea75e8f1
AleksandrGala1995 <95771059+AleksandrGala1995@users.noreply.github.com>
2021-12-13 14:31:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Форма</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+    Введіть ім'я українською: <input type="text" name="name"><br>
+    <input type="submit">
+</form>
+<?php
+if (!empty($_POST['name'])) {
+    $name = $_POST['name'];
+    $pattern = '/^\s*[аАбБвВгГҐґдДеЕєЄжЖзЗиИіІїЇйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩьЬюЮяЯ\'\-]+\s+[аАбБвВгГҐґдДеЕєЄжЖзЗиИіІїЇйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩьЬюЮяЯ\'\-]+\s+[аАбБвВгГҐґдДеЕєЄжЖзЗиИіІїЇйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩьЬюЮяЯ\'\-]+\s*$/u';
+    if (preg_match($pattern, $name)) {
+        
+        echo "Ім'я правильне";
+    } else {
+        
+         echo "Ім'я неправильне";
+    }
+}
+?>
+
+</body>
+</html>
